955e74e757c8613effc03627b8caebdf5701d059
Joshua kim <joshuakim.changer@gmail.com>
2019-01-06 22:01:02 +0900

    Add 'id' Column in Word to Keep Order of Excel Data by Sorting

--- data.py
+++ data.py
@@ -1,14 +1,18 @@
 class Word:
-    def __init__(self, word: str, meaning: str, wrong_nums: int):
+    def __init__(self, id: int, word: str, meaning: str, wrong_nums: int):
         """
         :param word: 단어
         :param meaning: 단어의 의미
         :param wrong_nums: 누적 틀린 횟수
         """
+        self.id = id
         self.word = word
         self.meaning = meaning
         self.wrong_nums = wrong_nums
         self.is_checked = False
+
+    def __str__(self):
+        return f"[#{self.id}] {self.word} / {self.wrong_nums}"
 
     def check_answer(self):
         print(f"meaning: {self.meaning}")
